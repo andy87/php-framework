4a845ff9d88f9572a1af2e1b2eb091c3f71f205a
and_y87 <kidin_a@list.ru>
2020-05-05 13:28:21 +0300

    add const

--- app/_/setups/const.php
+++ app/_/setups/const.php
@@ -14,6 +14,8 @@ define('DEFAULT_ACTION', "index");
 define('DEFAULT_RESPONSE', "text/html");
 define('DEFAULT_CHARSET', "utf-8");
 define('DEFAULT_LANG', "ru");
+define('DEFAULT_CODE', 200);
+
 
 define('CONTROLLER_SUFFIX', "Controller");
 define('CONTROLLER_NAMESPACE', "app\\controllers\\");
@@ -22,11 +24,12 @@ define('ACTION_PREFIX', "action");
 define('ACTION_ERROR', "error");
 
 
-define('TEMPLATE_ERROR', 'app/_/templates/error' . PHP );
+define('TEMPLATE_ERROR', '@root/app/_/templates/error' . PHP );
 define('TEMPLATE_FORMAT', PHP );
 
 define('AJAX', "XMLHttpRequest");
 
 define('METHOD_LIST', ['POST','GET','PUT','UPDATE','DELETE','HEAD','CONNECT','OPTIONS','TRACE','PATCH' ]);
 
-define('DIRECTORY_LIST', ['config','controllers','models','static','views']);
+define('DIRECTORY_APP', ['config','controllers','models','static','views']);
+define('DIRECTORY_STATIC', ['css','js', 'img', 'docs', 'fonts']);
