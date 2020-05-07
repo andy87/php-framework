91f20f003699a3d5f2921be309c6346ee076a7a6
and_y87 <kidin_a@list.ru>
2020-05-07 18:25:48 +0300

    codeStyle

--- app/config/routes.php
+++ app/config/routes.php
@@ -2,15 +2,17 @@
 
 $routes = [
     // URI( url )             Controller/action
-    'sign-up'           => 'site/sign-up',
-    'login'             => 'site/login',
-    'contact'           => 'site/contact',
-    'gradient-color'        => 'color-gradient/linear-gradient',
-    'gradient-color/json'   => 'color-gradient/return-json',
-    'gradient-color/json2'   => 'color-gradient/return-json2',
-    'gradient-color/2'      => 'color-gradient/radial-gradient',
+    'sign-up'                       => 'site/sign-up',
+    'login'                         => 'site/login',
+    'contact'                       => 'site/contact',
+    'gradient-color'                => 'color-gradient/linear-gradient',
+    'gradient-color/json'           => 'color-gradient/return-json',
+    'gradient-color/json2'          => 'color-gradient/return-json2',
+    'gradient-color/2'              => 'color-gradient/radial-gradient',
     //TODO: сделать возможность аргументов в URI
-    'gradient-color/<action>' => 'color-gradient/<action>',
+    'gradient-color/<action>'       => 'color-gradient/<action>',
     //'gradient-color/<from:[\w\d]>/<to:[\w\d]>' => 'color-gradient/linear-gradient',
-    '/'                  => 'site/index',
+    '<action>'                      => 'site/<action>',
+    '<controller>/<action>'         => '<controller>/<action>',
+    '/'                             => 'site/index',
 ];
